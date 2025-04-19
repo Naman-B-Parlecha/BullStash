035901e4618f2659c9232c9d445bbe8e6afdfbb6
Naman-B-Parlecha <namanparlecha@gmail.com>
2025-04-19 19:41:10 +0530

    feat: Added Notify CMD

---
+++ cmd/notify.go
@@ -0,0 +1,66 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+// notifyCmd represents the notify command
+var notifyCmd = &cobra.Command{
+	Use:   "notify",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		discordWebhook, _ := cmd.Flags().GetString("discord")
+		if discordWebhook == "" {
+			fmt.Println("Please provide a Discord webhook URL.")
+			return
+		}
+
+		filecontent, err := os.ReadFile(".env")
+		if err != nil {
+			fmt.Println("Error reading .env file:", err)
+			return
+		}
+		content := string(filecontent)
+		if len(content) > 0 && !strings.HasSuffix(content, "\n") {
+			content += "\n"
+		}
+		content += "\nDISCORD_WEBHOOK_URL=" + discordWebhook + "\n"
+
+		err = os.WriteFile(".env", []byte(content), 0644)
+		if err != nil {
+			fmt.Println("Error writing to .env file:", err)
+			return
+		}
+
+		fmt.Printf("Notification will be sent to Discord webhook: %s\n", discordWebhook)
+		fmt.Println("WebHook added successfully!")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(notifyCmd)
+	notifyCmd.Flags().String("discord", "", "Discord webhook URL")
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// notifyCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// notifyCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
